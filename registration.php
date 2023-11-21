2488925247eb146b770a7229bd4f6c2922a287d3
dayanandyadav205 <dayanandyadav205@gmail.com>
2023-11-21 19:36:33 +0530

    update

--- registration.php
+++ registration.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registration</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css" type="text/css">
 
 </head>
@@ -89,7 +90,7 @@
             <div class="form-group">
                 <input type="password" class="form-control" name="repeat_password" placeholder="repeat password">
             </div>
-            <div class="form-group">
+            <div class="form-btn">
                 <input type="submit" class="btn btn-primary" name="submit" value="Register">
             </div>
         </form>
