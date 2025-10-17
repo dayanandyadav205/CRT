b59d41308eb09fe818983a0c6159ca470bdb7beb
dayanandyadav205 <dayanandyadav205@gmail.com>
2025-10-17 19:46:32 +0530

    update

--- js/header.js
+++ js/header.js
@@ -5,16 +5,16 @@ class MyHeader extends HTMLElement {
 
   <!-- Header -->
   <div class="header">
-    <div class="header-left">
+    <div>
       <img src="images/cdgi.jpg" alt="CDGI Logo" width="100" height="100" />
     </div>
 
-    <div class="header-center">
+    <div>
       <h1>Chameli Devi Group of Institutions, Indore</h1>
       <h2>Web Development</h2>
     </div>
 
-    <div class="header-right">
+    <div>
       <img src="images/Dayanand.jpg" alt="GitHub" width="100" height="100" />
     </div>
   </div>
@@ -23,7 +23,7 @@ class MyHeader extends HTMLElement {
   <div class="navbar">
       <a href="index.html"><img src="images/home.ico" alt=""></a>
       <a href="contact.html">Contact</a>
-      <a href="login_admin.html">Admin Login</a>
+      <a href="login_admin.html" style="justify-content: right;">Admin Login</a>
       <a href="login_user.html">User Login</a>
       <a href="form.html">Sign Up</a>
   </div>
