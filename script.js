8e72dc1a9eb211c6e4da8e362c97943b23a49e80
dayanandyadav205 <dayanandyadav205@gmail.com>
2025-12-27 14:51:27 +0530

    updated

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ async function loadComponent(id, file) {
 loadComponent("header", "/header.html");     // load header.html into <div id="header">
 loadComponent("footer", "/footer.html");     // load footer.html into <div id="footer">
 
+
 //Calculator
 function addition() {
   let num1 = parseFloat(document.getElementById("num1").value);
@@ -46,6 +47,7 @@ function division() {
 
   document.getElementById("result").innerHTML = div;
 }
+
 
 //Check Password & Show Password
 function checkpwd() {
@@ -160,6 +162,7 @@ function scissor() {
   // document.writeln(`You select Scissor. Computer select ${computerMove}. Result is ${result}`);
 }
 
+
 //Rock Paper Scissor with Graphics
 function rockNew() {
 
@@ -205,12 +208,10 @@ function rockNew() {
     score.ties += 1;
   }
 
-
   localStorage.setItem('score', JSON.stringify(score));
 
   document.getElementById("demo").innerHTML = `You Select Rock. Computer Select ${computerMove}. ${result}. Wins: ${score.wins}. Losses: ${score.losses}. Ties: ${score.ties}`;
 }
-
 
 function paperNew() {
 
@@ -255,7 +256,6 @@ function paperNew() {
   } else if (result == "Tie") {
     score.ties += 1;
   }
-
 
   localStorage.setItem('score', JSON.stringify(score));
 
@@ -306,11 +306,11 @@ function scissorNew() {
     score.ties += 1;
   }
 
-
   localStorage.setItem('score', JSON.stringify(score));
 
   document.getElementById("demo").innerHTML = `You Select Rock. Computer Select ${computerMove}. ${result}. Wins: ${score.wins}. Losses: ${score.losses}. Ties: ${score.ties}`;
 }
+
 
 //Simple Quiz
 function quizResult() {
@@ -332,6 +332,62 @@ function quizResult() {
   document.getElementById("percentage").innerHTML = "Your percentage is: " + percentage;
 }
 
+
+
+//Shopping cart
+//cart
+let cart = []; //global
+
+function addCart(item, price) {
+  let existingItem = cart.find(c => c.item === item); //local variable
+
+  if (existingItem) {
+    cart[i].quantity += 1;
+  }
+  cart.push({ item, price, quantity: 1 });
+  renderCart(); //nested function
+}
+
+function renderCart() {
+  let cartItem = document.getElementById('cartItems');
+  cartItem.innerHTML = "";
+
+  let totalPrice = document.getElementById('totalPrice');
+
+  let total = 0;
+
+  for (let i = 0; i <= cart.length; i++) {
+    let li = document.createElement('li');
+    li.textContent = `${cart[i].item} (x ${cart[i].quantity}): ${cart[i].price * cart[i].quantity}`;
+    cartItem.appendChild(li);
+    total = total + cart[i].price * cart[i].quantity;
+
+    let btnIncrement = document.createElement('button');
+    btnIncrement.textContent = '+';
+    li.appendChild(btnIncrement);
+
+    btnIncrement.onclick = () => {
+      cart[i].quantity += 1;
+      renderCart();
+    }
+
+    let btnDecrement = document.createElement('button');
+    btnDecrement.textContent = '-';
+    li.appendChild(btnDecrement);
+
+    btnDecrement.onclick = () => {
+
+      if (cart[i].quantity > 1) {
+        cart[i].quantity -= 1;
+      } else {
+        cart.splice(i, 1);
+      }
+      renderCart();
+    }
+    totalPrice.innerHTML = 'Total Price: ' + total;
+  }
+
+}
 
 
 
