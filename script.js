618b229dd86daa2c5b5a19637e8573c1d03820e6
dayanandyadav205 <dayanandyadav205@gmail.com>
2025-12-27 14:42:19 +0530

    updated

--- script.js
+++ script.js
@@ -332,78 +332,8 @@ function quizResult() {
   document.getElementById("percentage").innerHTML = "Your percentage is: " + percentage;
 }
 
-//To Do List Application
-let items = JSON.parse(localStorage.getItem("items")) || [];
-
-function saveItems() {
-  localStorage.setItem("items", JSON.stringify(items));
-}
-
-function addItem() {
-  let input = document.getElementById("inputItem").value;
-  document.getElementById("inputItem").value = "";
-
-  if (input === "") {
-    alert("Please enter an item");
-  } else {
-    items.push(input);
-  }
-  // console.log(input);
-  saveItems()
-  renderItems();
-}
-
-function deleteItem(index) {
-  items.splice(index, 1);
-  saveItems()
-  renderItems();
-}
-
-function editItem(index) {
-  let newValue = prompt("Edit Item:", items[index]);
-  if (newValue !== '' && newValue !== null) {
-    items[index] = newValue;
-  }
-  saveItems()
-  renderItems();
-}
-
-function renderItems() {
-  let itemList = document.getElementById("toDoList");
-  document.getElementById("toDoList").innerHTML = "";
-
-  items.forEach((items, index) => {
-    let li = document.createElement('li');
-    li.textContent = items;
-    itemList.appendChild(li);
-    console.log(itemList);
-
-    let deleteBtn = document.createElement('button');
-    deleteBtn.textContent = 'X';
-    li.appendChild(deleteBtn);
-    deleteBtn.addEventListener('click', () => deleteItem(index));
-    // console.log(items);
-
-    let editBtn = document.createElement('button');
-    editBtn.textContent = 'Edit';
-    // console.log(editBtn);
-    li.appendChild(editBtn);
-    editBtn.addEventListener('click', () => editItem(index));
 
 
-  });
-
-}
-renderItems();
-
-//Temperature Converter
-let tempCelcius = [0, 14, 18, 23, 39, 47];
-
-let tempFehrenheit = tempCelcius.map(c => ((c * 9 / 5) + 32));
-
-tempFehrenheit.forEach((tempFehrenheit) => {
-  console.log(tempFehrenheit)
-});
 
 
 
