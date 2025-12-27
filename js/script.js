7be1b862cb5af62e9f30974b881ddee4548f62c3
dayanandyadav205 <dayanandyadav205@gmail.com>
2025-12-27 11:31:36 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -7,31 +7,4 @@
 
     loadComponent("header", "/header.html");     // load header.html into <div id="header">
     loadComponent("footer", "/footer.html");     // load footer.html into <div id="footer">
-  
-
-//  //js for pawd show
-//         function showpwd() {
-//             let pwd = document.getElementById('pwd');
-
-//             if (pwd.type == 'password') {
-//                 pwd.type = 'text';
-//             }
-//             else {
-//                 pwd.type = 'password';
-//             }
-//         }
-
-//         // js for pwd match
-//         function chkpwd() {
-//             let p1 = document.f1.password.value;
-//             let p2 = document.f1.cpassword.value;
-
-//             if (p1 == p2) {
-//                 return true;
-//             }
-//             else {
-//                 alert("Password Mismach");
-//                 return false;
-//             }
-//         }
-
+    
